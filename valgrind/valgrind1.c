f77b7b416c2c4f01634e5a67a01f494b67f84db0
Filippo Scudella <filippo.scudella@studenti.unipd.it>
2024-05-12 21:00:30 +0200

    Valgrind segnalava che alla riga 9 era presente una scrittura invalida. In particolare x[10] = 0 veniva scritto in memoria andando a sforare i 40 bytes allocati con la malloc(). Ho quindi modificato x[10] = 0 con x[9] = 0. Aggiunto infine il controllo per sapere se l'allocazione della memoria è andata a buon fine

--- valgrind/valgrind1.c
+++ valgrind/valgrind1.c
@@ -2,11 +2,16 @@
 // In questo file ci sono due problemi
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void f(void)
 {
 	int* x = malloc(10 * sizeof(int));
-	x[10] = 0;
+	if(x == NULL){
+		printf("Errore: malloc() fallita in f()\n");
+		exit(EXIT_FAILURE);
+	}
+	x[9] = 0;
 }
 
 int main(void)
